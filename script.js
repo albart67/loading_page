6976009c18e526cbceb2244feb0719292b83f2e3
ALAIN BARTHELME <60420847+albart67@users.noreply.github.com>
2021-01-12 00:04:21 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+const loader = document.querySelector(".loader");
+
+// window.addEventListener('load', () => {
+//     x = loader.classList.add('fondu-out');
+// })
+
+function LoadPage() { 
+  loader.classList.add("fondu-out")
+  };
+
+setTimeout(LoadPage, 5000); 
